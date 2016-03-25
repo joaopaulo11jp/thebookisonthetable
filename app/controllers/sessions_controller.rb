8353368b778d18657114a3a91c80957f403d188a
João Paulo <joaopaulo11jp@gmail.com>
2016-03-25 23:56:07 +0000

    Session and App Index

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  include SessionsHelper
+
+  def new
+    render layout: 'auth'
+  end
+
+  def create
+    @user = User.find_by(email: params[:session][:email])
+    if @user && @user.authenticate(params[:session][:password]) then
+      log_in @user
+      redirect_to @user
+    else
+      flash.now[:danger] = t('controllers.sessions.flash.failure')
+      render 'new',layout:'auth'
+    end
+
+
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end
